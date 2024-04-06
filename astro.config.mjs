87baf9c08fc2cce23f435d38a8de901cf7b8a46b
Jose Delgado <j3leonardodelgado@gmail.com>
2024-04-05 21:35:35 -0400

    new time zone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
-import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    scripts: [
-        {
-            src: '/path/to/countdown.js',
-            type: 'module',
-        },
-    ],
+    scripts: [{ src: '/path/to/countdown.js', type: 'module' }],
     output: 'hybrid',
-    adapter: netlify(),
+    adapter: vercel(),
 });
