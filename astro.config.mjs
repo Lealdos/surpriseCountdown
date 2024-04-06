0f454eb1af6bc3f3066d603196c6d17e3467644d
Jose Delgado <j3leonardodelgado@gmail.com>
2024-04-05 21:23:41 -0400

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
-
 import react from '@astrojs/react';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    scripts: [{ src: '/path/to/countdown.js', type: 'module' }],
+    scripts: [
+        {
+            src: '/path/to/countdown.js',
+            type: 'module',
+        },
+    ],
     output: 'hybrid',
-    adapter: vercel(),
+    adapter: netlify(),
 });
